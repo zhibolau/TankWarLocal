e318e4823f90fe6f5517712bc11e45e829e7fe8f
Zhibo Liu <zhibolau@gmail.com>
2016-07-20 00:10:20 -0700

    added java doc for tank war local

---
+++ TankWar2.6/src/com/zhiboliu/tankWarLocal/Blood.java
@@ -0,0 +1,59 @@
+package com.zhiboliu.tankWarLocal;
+
+import java.awt.*;
+
+/**
+ * Created by zhiboliu on 16-7-19.
+ */
+public class Blood {
+    int x,y,w,h;
+    TankClient tc;
+
+    public boolean isLive() {
+        return live;
+    }
+
+    public void setLive(boolean live) {
+        this.live = live;
+    }
+
+    private boolean live = true;
+
+    int step = 0;
+
+    private int[][] pos = {
+        {350,300}, {360, 300},{375,275},{400, 200},{359, 269}, {367,298},{349,279}
+    };
+
+    public Blood(){
+        x = pos[0][0];
+        y = pos[0][1];
+        w = h = 15;
+
+    }
+
+    public void draw(Graphics g){
+        if(!live){
+            return;
+        }
+        Color c = g.getColor();
+        g.setColor(Color.MAGENTA);
+        g.fillRect(x, y, w, h);
+        g.setColor(c);
+
+        move();
+    }
+
+    public void move(){
+        step++;
+        if(step == pos.length){
+            step = 0;
+        }
+        x = pos[step][0];
+        y = pos[step][1];
+    }
+
+    public Rectangle getRect(){
+        return new Rectangle(x, y, w, h);
+    }
+}
---
+++ TankWar2.6/src/com/zhiboliu/tankWarLocal/Wall.java
@@ -0,0 +1,29 @@
+package com.zhiboliu.tankWarLocal;
+
+import java.awt.*;
+
+/**
+ * Created by zhiboliu on 16-7-19.
+ */
+public class Wall {
+    int x, y, w, h;
+
+    TankClient tc;
+
+    public Wall(int x, int y, int w, int h, TankClient tc) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.tc = tc;
+    }
+
+    public void draw(Graphics g){
+        g.fillRect(x, y, w, h);
+    }
+
+    public Rectangle getRect(){
+        return new Rectangle(x, y, w, h);
+    }
+
+}
